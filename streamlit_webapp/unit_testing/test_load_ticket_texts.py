ee4b5a2a18b2489729f1ef86af2853fe50dbe220
YB <60541985+hanyu1774@users.noreply.github.com>
2026-07-24 13:22:42 +0200

    Delete streamlit_webapp/unit_testing directory

--- streamlit_webapp/unit_testing/test_load_ticket_texts.py
+++
@@ -1,8 +0,0 @@
-from flows.load_ticket_texts import LoadTicketTexts
-
-
-def test_loads_ticket_descriptions_with_category(seeded_engine):
-    df = LoadTicketTexts().run(seeded_engine)
-    assert len(df) == 3
-    assert "description" in df.columns
-    assert "error_category" in df.columns
